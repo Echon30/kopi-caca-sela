306de4df88bd85ddd9a070ddf573acd5f41f2868
Echon30 <marshandapatioran45@gmail.com>
2023-12-16 23:09:35 +0900

    changes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,67 @@
-// ketika hamburger menu di klik
+// toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
-document.querySelector("#hamburger-menu").onclick = () => {
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik di luar sidebar untuk menghilangkan navbar
-const hamburger = document.querySelector("#hamburger-menu");
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.getElementById("search-box");
+
+document.getElementById("search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.getElementById("shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar elemen
+const hm = document.getElementById("hamburger-menu");
+const sb = document.getElementById("search-button");
+const sc = document.getElementById("shopping-cart-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+// modal box
+const itemDetailModal = document.getElementById("item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
